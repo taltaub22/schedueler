98f12e4f03d4592530e8be9f0eabf7f08ff32754
Idan Stark <idanstark42@gmail.com>
2019-02-06 19:45:47 +0200

    another commit

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const Promise = require('bluebird')
 const fs = Promise.promisifyAll(require('fs'))
 
 const { scheduele } = require('./lib/schedueler')
+const { writeCSV } = require('./lib/csv_formatting')
 
 fs.readFileAsync(path.resolve(__dirname, '.', 'dev', 'input.json'), 'utf8').then(fileContent => {
 	const input = JSON.parse(fileContent)
 	const output = scheduele(input)
-	fs.writeFileAsync(path.resolve(__dirname, '.', 'dev', 'output.json'), JSON.stringify(output), 'utf8').then(() => { })
+	writeCSV(output, path.resolve(__dirname, '.', 'dev', 'output.csv'))
 })
